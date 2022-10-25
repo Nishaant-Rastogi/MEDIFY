16f9867e73b11407b6785f78957f5c4d9272e328
Nishaant-Rastogi <nishaant20091@iiitd.ac.in>
2022-10-25 12:09:16 +0530

    post

--- backend/frontend/src/pages/SignUp.js
+++ backend/frontend/src/pages/SignUp.js
@@ -3,9 +3,34 @@ import '../styles/signup.css'
 
 const SignUp = () => {
     const [signUpAsUser, setSignUpAsUser] = useState(true)
-    const [signUpAsOrganisation, setSignUpAsOrganisation] = useState(false)
 
+    const handleSignUpAsUser = (e) => {
+        console.log("called")
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                name: e.target.name.value,
+                dob: e.target.dob.value,
+                gender: e.target.gender.value,
+                address: e.target.address.value,
+                phoneNo: e.target.phoneNo.value,
+                aadharNo: e.target.aadharNo.value,
+                userType: e.target.userType.value,
+                email: e.target.email.value,
+                password: e.target.password.value,
+            }),
+        };
+        fetch('/api/create-user', requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+            });
 
+    }
+    const handleSignUpAsOrganization = () => {
+
+    }
     return (
         <div className='SIGNUP'>
             <img className="SIGNUPIMG" src="/static/images/Healthcare-portal.jpg" />
@@ -20,21 +45,21 @@ const SignUp = () => {
                 </div>
                 <div className='FORMCONTAINER'>
                     {signUpAsUser ?
-                        <form onSubmit={"handleSIGNUPUser"}>
+                        <form onSubmit={handleSignUpAsUser}>
                             <div className="form-group">
                                 <label html="exampleInputid1">Name</label>
-                                <input type="id" className="form-control" id="Customer_ID" aria-describedby="idHelp" placeholder="Enter Name" />
+                                <input type="id" className="form-control" name="name" aria-describedby="idHelp" placeholder="Enter Name" />
                                 {/* <small id="idHelp" className="form-text text-muted">We will never share your id with anyone else.</small> */}
                             </div>
                             <div style={{ display: 'flex' }}>
                                 <div className="form-group" style={{ marginRight: '20px' }}>
                                     <label html="exampleInputid1">Date of Birth</label>
-                                    <input type="date" className="form-control" id="DOB" aria-describedby="idHelp" placeholder="Enter DOB" />
+                                    <input type="date" className="form-control" name="dob" aria-describedby="idHelp" placeholder="Enter DOB" />
                                     {/* <small id="idHelp" className="form-text text-muted">We will never share your id with anyone else.</small> */}
                                 </div>
                                 <div className="form-group" aria-label="Default select example">
                                     <label html="exampleInputid1">Gender</label><br></br>
-                                    <select className="form-control">
+                                    <select className="form-control" name='gender'>
                                         <option selected>Select Gender</option>
                                         <option value="M">Male</option>
                                         <option value="F">Female</option>
@@ -45,24 +70,24 @@ const SignUp = () => {
                             </div>
                             <div className="form-group">
                                 <label html="exampleInputid1">Address</label>
-                                <input type="text" className="form-control" id="Address" aria-describedby="idHelp" placeholder="Enter Address" />
+                                <input type="text" className="form-control" name="address" aria-describedby="idHelp" placeholder="Enter Address" />
                                 {/* <small id="idHelp" className="form-text text-muted">We will never share your id with anyone else.</small> */}
                             </div>
                             <div style={{ display: 'flex' }}>
                                 <div className="form-group" style={{ marginRight: '20px' }}>
                                     <label html="exampleInputid1">Phone No</label>
-                                    <input type="text" className="form-control" id="Phone_No" aria-describedby="idHelp" placeholder="Enter Phone" />
+                                    <input type="text" className="form-control" name="phoneNo" aria-describedby="idHelp" placeholder="Enter Phone" />
                                     {/* <small id="idHelp" className="form-text text-muted">We will never share your id with anyone else.</small> */}
                                 </div>
                                 <div className="form-group">
                                     <label html="exampleInputid1">Aadhar No</label>
-                                    <input type="text" className="form-control" id="Aadhar_No" aria-describedby="idHelp" placeholder="Enter Aadhar" />
+                                    <input type="text" className="form-control" name="aadharNo" aria-describedby="idHelp" placeholder="Enter Aadhar" />
                                     {/* <small id="idHelp" className="form-text text-muted">We will never share your id with anyone else.</small> */}
                                 </div>
                             </div>
                             <div className="form-group" aria-label="Default select example">
                                 <label html="exampleInputid1">User Type</label><br></br>
-                                <select className="form-control">
+                                <select className="form-control" name="userType">
                                     <option selected>Select Type of User</option>
                                     <option value="U">Patient</option>
                                     <option value="D">Doctor</option>
@@ -70,13 +95,13 @@ const SignUp = () => {
                             </div>
                             <div className="form-group">
                                 <label html="exampleInputid1">Email</label>
-                                <input type="id" className="form-control" id="Email" aria-describedby="idHelp" placeholder="Enter Email" />
+                                <input type="id" className="form-control" name="email" aria-describedby="idHelp" placeholder="Enter Email" />
                                 {/* <small id="idHelp" className="form-text text-muted">We will never share your id with anyone else.</small> */}
                             </div>
                             <div style={{ display: 'flex' }}>
                                 <div className="form-group" style={{ marginRight: '20px' }}>
                                     <label html="exampleInputPassword1">Password</label>
-                                    <input type="password" className="form-control" id="Password" placeholder="Password" />
+                                    <input type="password" className="form-control" name="password" placeholder="Password" />
                                 </div>
                                 <div className="form-group">
                                     <label html="exampleInputPassword1">Confirm Password</label>
@@ -85,10 +110,10 @@ const SignUp = () => {
                             </div>
                             <button type="submit" className="btn COLOR SIGNUPB">Sign Up User</button>
                         </form> :
-                        <form onSubmit={"handleSIGNUPUser"}>
+                        <form onSubmit={handleSignUpAsOrganization}>
                             <div className="form-group">
                                 <label html="exampleInputid1">Name</label>
-                                <input type="id" className="form-control" id="Name" aria-describedby="idHelp" placeholder="Enter Name" />
+                                <input type="id" className="form-control" name="name" aria-describedby="idHelp" placeholder="Enter Name" />
                             </div>
                             <div className="form-group" aria-label="Default select example">
                                 <label html="exampleInputid1">Type Of Organisation</label><br></br>
@@ -101,31 +126,31 @@ const SignUp = () => {
                             </div>
                             <div className="form-group">
                                 <label html="exampleInputid1">License No</label>
-                                <input type="id" className="form-control" id="License" aria-describedby="idHelp" placeholder="Enter License No" />
+                                <input type="id" className="form-control" name="license" aria-describedby="idHelp" placeholder="Enter License No" />
                             </div>
                             <div className="form-group">
                                 <label html="exampleInputid1">Contact No</label>
-                                <input type="text" className="form-control" id="Phone_No" aria-describedby="idHelp" placeholder="Enter Phone" />
+                                <input type="text" className="form-control" name="phoneNo" aria-describedby="idHelp" placeholder="Enter Phone" />
                                 {/* <small id="idHelp" className="form-text text-muted">We will never share your id with anyone else.</small> */}
                             </div>
                             <div className="form-group">
                                 <label html="exampleInputid1">Address</label>
-                                <input type="text" className="form-control" id="Address" aria-describedby="idHelp" placeholder="Enter Address" />
+                                <input type="text" className="form-control" name="Address" aria-describedby="idHelp" placeholder="Enter Address" />
                                 {/* <small id="idHelp" className="form-text text-muted">We will never share your id with anyone else.</small> */}
                             </div>
                             <div className="form-group">
                                 <label html="exampleInputid1">Email</label>
-                                <input type="id" className="form-control" id="Email" aria-describedby="idHelp" placeholder="Enter Email" />
+                                <input type="id" className="form-control" name="email" aria-describedby="idHelp" placeholder="Enter Email" />
                                 {/* <small id="idHelp" className="form-text text-muted">We will never share your id with anyone else.</small> */}
                             </div>
                             <div style={{ display: 'flex' }}>
                                 <div className="form-group" style={{ marginRight: '20px' }}>
                                     <label html="exampleInputPassword1">Password</label>
-                                    <input type="password" className="form-control" id="Password" placeholder="Password" />
+                                    <input type="password" className="form-control" name="password" placeholder="Password" />
                                 </div>
                                 <div className="form-group">
                                     <label html="exampleInputPassword1">Confirm Password</label>
-                                    <input type="password" className="form-control" id="Password" placeholder="Confirm Password" />
+                                    <input type="password" className="form-control" name="password" placeholder="Confirm Password" />
                                 </div>
                             </div>
                             <button type="submit" className="btn COLOR SIGNUPB">Sign Up Organisation</button>
