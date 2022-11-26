59f0996a292263c534a8bcadd110da16afa8ed91
Nishaant-Rastogi <nishaant20091@iiitd.ac.in>
2022-11-27 01:10:01 +0530

    api remove

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('', include('api.urls')),
     path('', include('frontend.urls')),
 ]
